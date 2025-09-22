1beba20a140fc74d1cef3c3038f80c3bdb205459
otojbo <fepiks@gmail.com>
2025-09-21 23:50:33 -0500

    start project: add header and imports

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # INF 601 - Advanced Python
 # Illia Ivanov
 # Mini Project 2
+
+from __future__ import annotations
+
+from pathlib import Path
+from datetime import date
+import random
+from typing import Dict, Tuple, List
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from faker import Faker
